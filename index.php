306f702b63d90d137ee6cc8bdcfc1a6cea263722
Shahzad Bashir <shahzadbirmani8@gmail.com>
2020-07-08 23:31:58 +0500

    Adding Different Files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form Data Processing using Get/Post Method</title>
+</head>
+
+<body>
+    <form method="GET" action="Includes/index-form.php">
+        <label>First Name</label>
+        <input type="text" name="fname">
+        <br>
+        <label>Last Name</label>
+        <input type="text" name="lname">
+        <br>
+        <input type="submit" value="Submit" name="submit">
+    </form>
+</body>
+
+</html>
